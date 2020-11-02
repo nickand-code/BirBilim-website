dad7f25e15c7ccf1ffa6b37b02985adacbb336c5
nickand-code <71209484+nickand-code@users.noreply.github.com>
2020-11-02 23:06:10 +0600

    Add files via upload
    1. pagination changed
    2. 1 more <p>
    3. fa-primary color of icons changed to blue

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var mySwiper = new Swiper('.swiper-container', {
-    spaceBetween: 30,
+    spaceBetween: 0,
 
     autoplay: {
         delay: 2000,
@@ -10,7 +10,6 @@ var mySwiper = new Swiper('.swiper-container', {
     // If we need pagination
     pagination: {
         el: '.swiper-pagination',
-        dynamicBullets: true,
       },
   
     // Navigation arrows

0de012ba1f4bc0eda62d0b4e4e16746951965a93
nickand-code <71209484+nickand-code@users.noreply.github.com>
2021-01-31 19:49:43 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,54 +1,17 @@
-var mySwiper = new Swiper('.s1', {
+var mySwiper = new Swiper('.swiper-container', {
     spaceBetween: 0,
 
     autoplay: {
-        delay: 2000,
+        delay: 8000,
         disableOnInteraction: false,
     },
     
     loop: true,
     pagination: {
-        el: '.pag1',
+        el: '.swiper-pagination',
         type: 'bullets',
       },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
   });
-
-var mySwiperPrice1 = new Swiper('.s2', {
-  spaceBetween: 0,
-  autoplay: {
-      delay: 2000,
-      disableOnInteraction: false,
-  },
-  
-  loop: true,
-  pagination: {
-      el: '.pag2',
-      type: 'bullets',
-    },
-  navigation: {
-    nextEl: '.swiper-button-next-priceS',
-    prevEl: '.swiper-button-prev-priceS',
-  },
-});
-
-var mySwiperNames = new Swiper('.s4', {
-  spaceBetween: 0,
-  autoplay: {
-      delay: 2000,
-      disableOnInteraction: false,
-  },
-  // effect: 'fade',
-  
-  loop: true,
-  pagination: {
-      el: '.pag4',
-      type: 'bullets',
-    },
-});
 
 $('.parallax-window').parallax({imageSrc: 'images/parallax.png'});
 var myModal = new bootstrap.Modal(document.getElementById('myModal'), options)
